3830ea4862c3f8629c81928fb2001fe0523b75ea
BinaryITSolution <binesolution@gmail.com>
2021-01-20 17:36:37 +0700

    update package

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -22,13 +22,15 @@ class ComposerAutoloaderInit613fcf8a45d4cb02fa1c2bf29381f0d4
             return self::$loader;
         }
 
+        require __DIR__ . '/platform_check.php';
+
         spl_autoload_register(array('ComposerAutoloaderInit613fcf8a45d4cb02fa1c2bf29381f0d4', 'loadClassLoader'), true, true);
         self::$loader = $loader = new \Composer\Autoload\ClassLoader();
         spl_autoload_unregister(array('ComposerAutoloaderInit613fcf8a45d4cb02fa1c2bf29381f0d4', 'loadClassLoader'));
 
         $useStaticLoader = PHP_VERSION_ID >= 50600 && !defined('HHVM_VERSION') && (!function_exists('zend_loader_file_encoded') || !zend_loader_file_encoded());
         if ($useStaticLoader) {
-            require_once __DIR__ . '/autoload_static.php';
+            require __DIR__ . '/autoload_static.php';
 
             call_user_func(\Composer\Autoload\ComposerStaticInit613fcf8a45d4cb02fa1c2bf29381f0d4::getInitializer($loader));
         } else {
